4863b859422e1d0423a7ea897d0b84f14d958752
bonimat <m.boni@unibo.it>
2020-06-08 19:30:00 +0200

    first commit

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+exports.write = function (n) {
+    var result = "";
+    if (n % 3 === 0) {
+        result = "fizz";
+    }
+    if (n % 5 === 0) {
+        result += "buzz";
+    }
+    if (result==="") {
+        result = n;
+    }
+
+    return result;
+
+}
